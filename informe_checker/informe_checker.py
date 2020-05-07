7a7899b5b4c2de0dc775ae7b798e26979e993c8c
Javier López <javierlopez@uc.cl>
2020-05-07 17:47:08 -0400

    fix(checker): fetch with requests

--- informe_checker/informe_checker.py
+++ informe_checker/informe_checker.py
@@ -4,16 +4,20 @@ import pendulum
 import sys
 import os
 import os.path
+import requests
 
-source = ""
-for line in sys.stdin:
-    source += line
+GOV_URL = "https://www.gob.cl/coronavirus/cifrasoficiales/"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1",
+           "DNT": "1", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
 
-soup = bs.BeautifulSoup(source, features="html.parser")
+source = requests.get(GOV_URL, timeout=10, headers=headers)
+soup = bs.BeautifulSoup(source.content, features="html.parser")
 
 informes_table = soup.find(id="informes").find("table")
-last_informe_date_str = informes_table.find("tbody").find("tr").find("td").string
-last_informe_date = dateparser.parse(last_informe_date_str, languages=['es']).strftime('%Y-%m-%d')
+last_informe_date_str = informes_table.find(
+    "tbody").find("tr").find("td").string
+last_informe_date = dateparser.parse(last_informe_date_str, languages=[
+                                     'es']).strftime('%Y-%m-%d')
 
 if os.path.isfile("../informe_parser/input/tablas_informe_{}.pdf".format(last_informe_date)):
     sys.exit(1)
