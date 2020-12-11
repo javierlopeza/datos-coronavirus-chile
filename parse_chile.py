953e7be54ed85e3273082827df4fe33168a94854
Javier López A <javierlopez@javiers-mbp.lan>
2020-12-11 02:01:17 -0300

    feat: update data

--- parse_chile.py
+++ parse_chile.py
@@ -9,7 +9,7 @@ def clean_list_string_numbers(l):
             l[i] = l[i - 1]
     return l
 
-def write_values(filename, source):
+def write_values(filename, dates, source):
     with open('./raw_data/chile/' + filename, 'w', newline='') as csvfile:
         fieldnames = ['date', 'value', 'is_from_informe_epi']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -27,12 +27,17 @@ with open('./input_files/TotalesNacionales.csv', newline='') as csvfile:
             totales = clean_list_string_numbers(row[1:])
         if row[0] == "Fallecidos":
             fallecidos = clean_list_string_numbers(row[1:])
-        if row[0] == "Casos activos":
-            activos = clean_list_string_numbers(row[1:])
+        # if row[0] == "Casos activos":
+        #     activos = clean_list_string_numbers(row[1:])
 
-write_values("serie_confirmados_chile.csv", totales)
-write_values("serie_fallecidos_chile.csv", fallecidos)
-write_values("serie_activos_chile.csv", activos)
+write_values("serie_confirmados_chile.csv", dates, totales)
+write_values("serie_fallecidos_chile.csv", dates, fallecidos)
 
-
-
+with open('./input_files/activos_vs_recuperados.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    dates = []
+    activos = []
+    for row in reader:
+        dates.append(row['fecha_primeros_sintomas'])
+        activos.append(row['activos'])
+write_values("serie_activos_chile.csv", dates, activos)
--- parse_input_files.sh
+++ parse_input_files.sh
@@ -1,1 +1,1 @@
-python parse_chile.py && python parse_regiones.py && python parse_comunas.py
+python parse_chile.py && python parse_regiones.py && python parse_comunas.py && python data_organizer.py
